0778c038a755f8598fc9321e866c473c5d560c12
gouseshaik08 <gouse5884@gmail.com>
2025-01-12 14:29:08 +0530

    set the history but i did not get the complete understanding of keys.

--- tictoctoe/src/App.jsx
+++ tictoctoe/src/App.jsx
@@ -1,38 +1,111 @@
 import { useState } from "react";
 import './App.css'
 
-function Square(value){
-  const [value,setValue] = useState();
-  
-  function handleClick(){
-    setValue("X")
-  }
+function Square({value,onSquareClick}){
   return(
     <>
-    <button className="square" onClick={handleClick}>{value}</button>
+    <button className="square" onClick={onSquareClick}>{value}</button>
     </>
   )
 }
 
-export default function Board(){
-  const[squares,setSquares] = useState(Arrys(9).fill(null));
+function Board({xIsNext,squares,onPlay}){
+  function handleClick(i){
+    if(squares[i] || calculateWinner(squares)) return;
+    const nextSquares = squares.slice();
+    if(xIsNext){
+      nextSquares[i] = 'X';
+    }
+    else{
+      nextSquares[i] = 'O';
+    }
+    onPlay(nextSquares);
+  }
+  const winner = calculateWinner(squares);
+  let status;
+  if(winner){
+    status = 'winner : ' + winner; 
+  }else{
+    status = 'next player : ' + (xIsNext ? 'X' : 'O');
+  }
+
   return(
     <>
+    <div className="status">{status}</div>
     <div className="board-row">
-      <Square value={squares[0]}/>
-      <Square value={squares[1]}/>
-      <Square value={squares[2]}/>
+      <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+      <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+      <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+    </div>  
+    <div className="board-row">
+      <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+      <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+      <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
     </div>
     <div className="board-row">
-      <Square value={squares[3]}/>
-      <Square value={squares[4]}/>
-      <Square value={squares[5]}/>
-    </div>
-    <div className="board-row">
-      <Square value={squares[6]}/>
-      <Square value={squares[7]}/>
-      <Square value={squares[8]}/>
+      <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+      <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+      <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
     </div>
     </>
   )
+}
+export default function Game(){
+  const[history,setHistory] = useState([Array(9).fill(null)]);
+  const[currMove,setCurrMove] = useState(0);
+  const currSquares = history[currMove];
+  const xIsNext = currMove%2 === 0;
+  function handlePlay(nextSquares){
+    const nextHistory = [...history.slice(0,currMove+1),nextSquares];
+    setHistory(nextHistory);
+    setCurrMove(nextHistory.length-1);
+  }
+  function jumpTo(nextMove){
+    setCurrMove(nextMove);
+  }
+  const moves = history.map((squares,move) => {
+    let description;
+    if(move > 0){
+      description = 'Go to move #' +move;
+    }else{
+      description = 'Go to Game Start';
+    }
+    return(
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  });
+
+  return(
+    <>
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext = {xIsNext} squares = {currSquares} onPlay = {handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+    </>
+   )
+}
+function calculateWinner(squares){
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+  for(let i=0;i<lines.length;i++){
+    const [a,b,c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
 }
