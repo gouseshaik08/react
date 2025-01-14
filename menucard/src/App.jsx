ddd5e8509f6cd6763db28faecddf7812fbe57f61
gouseshaik08 <gouse5884@gmail.com>
2025-01-14 11:21:24 +0530

    just a static display created

--- menucard/src/App.jsx
+++ menucard/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import FilterableProductTable from './FilterableProductTable'
 
 function App() {
   return (
     <>
     <h1>Gouse Designs From Scratch
     </h1>
+    <FilterableProductTable/>
     </>
   )
 }
